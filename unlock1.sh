098a3d21c7c1395b08cfc8e67ce644054638ff8b
khalidrash <58953392+khalidrash@users.noreply.github.com>
2019-12-16 22:16:30 +0200

    Update and rename unlock.sh to unlock1.sh

--- unlock1.sh
+++ unlock1.sh
@@ -30,7 +30,7 @@
 # 4 5 6
 # 7 8 9
 
-PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
+PATTERN="3 2 5 8 7" # The unlock pattern to draw, space seperated
 
 COL_1=166                   # X coordinate of column 1 (in pixels)
 COL_2=384                   # X coordinate of column 2 (in pixels)
@@ -40,7 +40,7 @@ ROW_1=553                   # Y coordinate of row 1 (in pixels)
 ROW_2=738                   # Y coordinate of row 2 (in pixels)
 ROW_3=984                   # Y coordinate of row 3 (in pixels)
 
-MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
+MULTIPLIER=2.1                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
                             # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
 
 WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
